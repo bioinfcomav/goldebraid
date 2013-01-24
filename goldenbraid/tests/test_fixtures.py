2616caa75e59118ba6de246770d4a2494f1ca7c6
Peio <pziarsolo@upv.es>
2013-01-24 11:47:32 +0100

    added featureprop fixtures

--- goldenbraid/tests/test_fixtures.py
+++ goldenbraid/tests/test_fixtures.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from goldenbraid.models import Db
+from goldenbraid.settings import DB
 
-FIXTURES_TO_LOAD = ['db', 'cv', 'cvterm', 'dbxref', 'feature']
-#                    'feature_cvterm', 'featureprop',
+FIXTURES_TO_LOAD = ['db', 'cv', 'cvterm', 'dbxref', 'feature', 'featureprop']
+#                    'feature_cvterm', ,
 #                    'feature_dbxref',
 #                    ]
 
@@ -12,6 +13,7 @@ FIXTURES_TO_LOAD = ['gb_%s.json' % table for table in FIXTURES_TO_LOAD]
 class TestFixtures(TestCase):
     'It tests that we can load the fixtures'
     fixtures = FIXTURES_TO_LOAD
+    multi_db = True
 
     def test_initial(self):
         'It loads the fixtures.'
