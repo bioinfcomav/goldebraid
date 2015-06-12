185d4735978b81716f071bd6c703833f4f652333
Peio <pziarsolo@upv.es>
2015-06-12 09:48:35 +0200

    half baked api test

---
+++ goldenbraid/tests/test_api.py
@@ -0,0 +1,21 @@
+'''
+Created on 2015 eka. 1
+
+@author: peio
+'''
+from django.test.testcases import TestCase
+from goldenbraid.tests.test_fixtures import FIXTURES_TO_LOAD
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+
+
+class ApiViewTest(TestCase):
+    fixtures = FIXTURES_TO_LOAD
+
+    def test_keyfeat(self):
+        client = Client()
+        url = reverse('api_feature_key_elements')
+
+        response = client.get('api_feature_key_elements',
+                              data={'features': ['GB0130']})
+        print str(response)
