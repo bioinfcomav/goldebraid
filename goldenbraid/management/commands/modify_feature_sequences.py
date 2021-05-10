e3c6de3f9a1bb9c4af0884c8a4d5741eed005250
victor <victor.gcb@gmail.com>
2021-05-10 16:02:12 +0200

    modify secuences

--- goldenbraid/management/commands/modify_feature_sequences.py
+++ goldenbraid/management/commands/modify_feature_sequences.py
@@ -34,24 +34,30 @@ class Command(BaseCommand):
         #     raise CommandError(str(error))
 
 def change_feature_residues(feature, original_seq, modified_seq):
-    residues = feature.residues
-    matches = re.findall(original_seq.upper(), residues)
-    if len(matches) != 1:
-        return False
-    else:
-        feature.residues = residues.replace(original_seq.upper(), modified_seq.upper())
-        feature.save()
-        return True
+	pattern = '(?=({}))'.format(original_seq.upper())
+	regexp = re.compile(pattern)
+	residues = feature.residues.upper()
+	search = regexp.finditer(residues)
+	results = [match.group(1) for match in search]
+	if len(results) > 1: 
+		return "More than one region to change"
+	elif len(results) == 0:
+		return "Seq not found"
+	else:
+		feature.residues = residues.replace(original_seq.upper(), modified_seq.upper())
+		feature.save()
+		return "OK"
 
 def change_feature_genbank(feature, original_seq, modified_seq):
-    try:
-        genbank_fpath = os.path.join(settings.MEDIA_ROOT, str(feature.genbank_file))
-        genbank_record = SeqIO.read(genbank_fpath, "gb")
-        genbank_record.seq = Seq(str(genbank_record.seq).replace(original_seq.upper(), modified_seq.upper()), generic_nucleotide)
-        SeqIO.write(genbank_record, open(genbank_fpath, "w"), "genbank")
-        return True
-    except:
-        return False
+	try:
+		genbank_fpath = os.path.join(settings.MEDIA_ROOT, str(feature.genbank_file))
+		genbank_record = SeqIO.read(genbank_fpath, "gb")
+		print(genbank_record)
+		genbank_record.seq = Seq(str(genbank_record.seq).replace(original_seq.upper(), modified_seq.upper()), generic_nucleotide)
+		SeqIO.write(genbank_record, open(genbank_fpath, "w"), "genbank")
+		return "Genbank modified"
+	except:
+		return "Error changing genbank"
 
 
 def get_users(users):
@@ -67,6 +73,7 @@ def run_command(fhand):
     report = {}
     features_to_change = []
     features_to_check = []
+    total_features = []
     for genbank in csv.DictReader(fhand, delimiter="\t"):
         uniquename = genbank["genbank"]
         original_seq = genbank["original_sequence"]
@@ -81,32 +88,33 @@ def run_command(fhand):
             feature = features_to_check.pop(0)
             related_features = FeatureRelationship.objects.filter(Q(subject__uniquename=feature.uniquename))
             for related_feature in related_features:
-                feature = related_feature.object
-                feature_owner = str(feature.owner)
-                if users_to_propagate == "NO":
-                    if feature_owner == owner:
-                        features_to_change.append(feature)
-                        features_to_check.append(feature)
-                elif users_to_propagate == "ALL":
-                    features_to_change.append(feature)
-                    features_to_check.append(feature)
-                elif feature_owner == owner or feature_owner in users_to_propagate:
-                    features_to_change.append(feature)
-                    features_to_check.append(feature)
-                else:
-                    continue
+            	total_features.append(related_feature)
+            	feature = related_feature.object
+            	feature_owner = str(feature.owner)
+            	print(users_to_propagate)
+            	if users_to_propagate == "NO":
+            		if feature_owner == owner:
+            			features_to_change.append(feature)
+            			features_to_check.append(feature)
+            	elif users_to_propagate == "ALL":
+            		features_to_change.append(feature)
+            		features_to_check.append(feature)
+            	elif feature_owner == owner or feature_owner in users_to_propagate:
+            		features_to_change.append(feature)
+            		features_to_check.append(feature)
+            	else:
+                	continue
+    print([related_feature.object.uniquename for feature in total_features])
     print(len(features_to_change))
     for feature in features_to_change:
         uniquename = feature.uniquename
-        if change_feature_residues(feature, original_seq, modified_seq):
-            report[uniquename] = {"residues": "OK"}
+        check_change = change_feature_residues(feature, original_seq, modified_seq) 
+        report[uniquename] = {"residues": check_change}
+        if report[uniquename]["residues"] == "OK":
+        	check_genbank = change_feature_genbank(feature, original_seq, modified_seq)
+        	report[uniquename]["genbank"] = check_genbank
         else:
-            report[uniquename] = {"residues": "Error"}
-        if change_feature_genbank(feature, original_seq, modified_seq):
-            report[uniquename]["genbank"] = "OK"
-        else:
-            report[uniquename]["genbank"] = "Error"
-
+        	report[uniquename]["genbank"] = "Not Done"
     print("feature\tresidues\tgenbank")
     for feature in report:
         line = "{}\t{}\t{}".format(feature, report[feature]["residues"], report[feature]["genbank"])
